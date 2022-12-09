05ad9fcccf97450534745f02db9adea347dcdcd1
Michal Javor <javor.mi.2020@skola.ssps.cz>
2022-12-09 16:53:05 +0100

    added functions: logSolution and crlfBullshit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ exports.checkIntegrity = function (input, sol1, sol2, func1, func2) {
         console.log(func2(input));
     }
 }
-exports.checkTime = function (input, times, func1, func2) {
+exports.checkTime = function (input, func1, func2, times = 1000) {
     console.time("sol1");
     for (let i = 0; i < times; i++) {
         func1(input);
@@ -23,4 +23,14 @@ exports.checkTime = function (input, times, func1, func2) {
         func2(input);
     }
     console.timeEnd("sol2");
+}
+exports.logSolution = function (input, func1, func2) {
+    console.log("sol1: ", func1(input));
+    console.log("sol2: ", func2(input));
+}
+exports.crlfBullshit = function (arrayToBeCleansed) {
+    for (i in arrayToBeCleansed) {
+        arrayToBeCleansed[i] = arrayToBeCleansed[i].replace('\r', '');
+    }
+    return arrayToBeCleansed;
 }
